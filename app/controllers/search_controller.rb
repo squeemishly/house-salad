f7420daf78acfb51e7e4a19e14754e29b2791946
squeemishly <squeemishly@gmail.com>
2017-07-12 11:02:42 -0600

    trial and error

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,11 @@
 class SearchController < ApplicationController
   def index
-    # search = Search.get_info
+    search = Search.get_info
 
     conn = Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
-    response = conn.get
+    response = conn.get do |req|
+      req.headers['X-API-Key'] = "#{ENV["propublica_api_key"]}"
+    end
     @result = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
