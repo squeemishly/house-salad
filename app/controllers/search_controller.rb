a6046507bb42000c9fbfc1dddcf823603b66e05d
squeemishly <squeemishly@gmail.com>
2017-07-12 10:15:16 -0600

    result count showing on search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def index
-    response = Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
-    binding.pry
-
+    conn = Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
+    response = conn.get
+    @result = JSON.parse(response.body, symbolize_names: true)[:results]
+    # binding.pry
   end
 end
