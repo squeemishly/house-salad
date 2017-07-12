eac1cc63ebf54fca7f84a5f06fd06ee518a958db
squeemishly <squeemishly@gmail.com>
2017-07-12 11:09:11 -0600

    page now loads on the website

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,10 @@
 class SearchController < ApplicationController
   def index
-    # search = Search.get_info
-
     conn = Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
     response = conn.get do |req|
       req.headers['X-API-Key'] = "#{ENV["propublica_api_key"]}"
     end
+    binding.pry
     @result = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,5 +1,5 @@
 class Search < ApplicationRecord
-  def get_info
+  def self.get_info
     Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
   end
 end
