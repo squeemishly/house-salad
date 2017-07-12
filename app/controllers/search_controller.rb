248be258673af27deac435240469974b4a91a208
squeemishly <squeemishly@gmail.com>
2017-07-12 10:26:47 -0600

    test passes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def index
+    search = Search.get_info
+
     conn = Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
     response = conn.get
     @result = JSON.parse(response.body, symbolize_names: true)[:results]
-    # binding.pry
   end
 end
