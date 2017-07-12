afb11813fa0114b6e57d8b8431973361cd293143
squeemishly <squeemishly@gmail.com>
2017-07-12 11:00:20 -0600

    passing API key on get request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,9 @@ class SearchController < ApplicationController
     # search = Search.get_info
 
     conn = Faraday.new :url => "https://api.propublica.org/congress/v1/members/house/CO/current.json", :headers => {"X-API-Key" => ENV["propublica_api_key"]}
-    response = conn.get
+    response = conn.get do |req|
+      req.headers['X-API-Key'] = "#{ENV["propublica_api_key"]}"
+    end
     @result = JSON.parse(response.body, symbolize_names: true)[:results]
   end
 end
